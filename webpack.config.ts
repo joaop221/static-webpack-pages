54ce9b82addaeac55f2687678af67dc113e2fd05
joao.garcia <joao.garcia@iteris.com.br>
2019-07-13 20:17:52 -0300

    add features and tools to webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,6 +4,18 @@ import webpack from 'webpack';
 const config: webpack.Configuration = {
     mode: 'production',
     entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: '/\.spec.ts$/',
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.bundle.js'
