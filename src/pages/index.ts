898332569b218b3ec118fd4a5f17ac20335f27d3
joao.garcia <joao.garcia@iteris.com.br>
2019-07-29 08:00:32 -0300

    funcions examples

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,13 @@
+import $ from 'jquery';
+
 import './index.scss';
+
+const listItens = () => [{name: 'lists', href: 'lists.html'}];
+
+const iteractItens = () => listItens().map((item) => `<a href="${item.href}>${item.name}</a>`);
+
+const appendLinks = () => {
+    $('#pages').append(''.concat(...iteractItens()));
+};
+
+appendLinks();
